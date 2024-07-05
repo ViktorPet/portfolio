f94b3aab88fc43ad6a5f628e8fb77f7334a182aa
ViktorPet <viktor.petrovvt@gmail.comgit config --global user.name ViktorPetgit config --global user.email viktor.petrovvt@gmail.com>
2024-07-05 12:23:56 +0300

    Render app titles

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import streamlit as st
 
+import pandas 
+
 st.set_page_config(layout="wide")
 
 col1, col2 = st.columns(2)
@@ -12,9 +14,20 @@ with col2:
     content = """ Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing 
     Lorem Ipsum passages, and more recently with desktop publishing software 
     like Aldus PageMaker including versions of Lorem Ipsum."""
-    st.info(content)
-    
-    
+    st.info(content) 
+      
+      
 st.subheader("Bellow you can find some of the apps built in Python. Feel free to contact me")
+
+col3, col4 = st.columns(2)
+
+df = pandas.read_csv("data.csv", sep=";")
+
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
 
 
